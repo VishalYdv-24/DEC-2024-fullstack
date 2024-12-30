ad2b1ee3b9e05a1fdbb167f78a3ffd140b6e1e7d
Vishal <vishal2004500@gmail.com>
2024-12-30 21:53:40 +0530

    Lecture 5:Functions and Methods

--- javascript/script.js
+++ javascript/script.js
@@ -1,39 +1,158 @@
-//Lecture 4
-//Arrays And Methods
-
-let marks = [25,15,12,42,55,5];
-// for loop
-for(let i = 0;i<marks.length;i++){
-    console.log(marks[i]);
+// Function & Methods
+/*Syntax:
+function functionName(){
+    write code here
 }
 
-// for-in
-for(i in marks){
-    console.log(i);
+to call function:
+
+functionName();
+*/
+function printinfo(name){
+    console.log(name,"Hello From Function block.");
+}
+function sum(num){
+    let s = 10;
+    s += num;
+    return s;
+}
+let num = 5;
+let name = "Vishal";
+printinfo(name);
+console.log(sum(num));
+
+// Arrow function : shorter form of function
+// Syntax : const functionName = (parameter1,2,3...) => {
+    // wirte code here
+// }
+
+const sum2 = (num) => {
+    return num+1;
+}
+console.log(sum2);
+console.log(sum2(num));
+
+// return number of vowels from string
+let str = "aeiouaeiouiiii";
+
+// using normal fuctions
+// function vowels(str){
+//     let vowelnum = 0;
+//     for(let i=0;i<str.length;i++){
+//         if('a' == str[i]){
+//             vowelnum++;
+//         }
+//         else if('e' == str[i]){
+//             vowelnum++;
+//         }
+//         else if('i' == str[i]){
+//             vowelnum++;
+//         }
+//         else if('o' == str[i]){
+//             vowelnum++;
+//         }
+//         else if('u' == str[i]){
+//             vowelnum++;
+//         }
+//     }
+//     return vowelnum;
+// }
+
+// using arrow function
+let vowels = (str) => {
+    let vowelnum = 0;
+    for(const char of str){
+        if(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
+            vowelnum++;
+        }
+    }
+    return vowelnum;
 }
 
-// for-of (better use for-of for array)
-for(i of marks){
-    console.log(i);
-}
+console.log(vowels(str));
 
-// practice: to find average of array nums
-let arrNum = [12,45,26,35,15,25];
-let sum = 0;
-for(let i=0;i<arrNum.length;i++){
-    sum += arrNum[i];
-}
-let avg = sum/arrNum.length;
-console.log(`Average : ${avg}`);
 
-// arr = [250,645,300,900,50] replace this numbers after giving 10% off
-let arr = [250,645,300,900,50];
-for(let i=0;i<arr.length;i++){
-    arr[i] -= (arr[i]*0.1);
-}
-console.log(arr);
+// forEach Loop in Arrays
+// Syntax : arr.forEach(callBackFunction)   callBackFunction : here,it is a function to execute for each elemnet in the array   *A callBackFunction passed as an argument to another function
+/*
+arr.forEach((value,index,array) =>{
+    console.log(val);
+})
+*/
 
-// Array Methods:
-// push():add to end    pop():delete from end & return      toString():converts array to string
-arr.toString();
-console.log(typeof arr[0]);
+let arr = ["mumbai","delhi","pune","banglore"];
+arr.forEach(function(val,indx,arr){
+    console.log(val,indx,arr);
+})
+
+arr.forEach((val,indx,arr) => {
+    console.log(val.toUpperCase(),indx,arr);
+})
+
+let numArr = [1,2,3,4,5,6,7,8,9];
+numArr.forEach((val) => {
+    console.log(`${val}:${val*val}`);
+})
+
+// MAP METHOD 
+/*
+Creats a new array with results of some operations. The value its callback returns are used to form new array
+syntax:
+arr.map(callbackfnx(value,index,array))
+*/
+
+let newArr = numArr.map((val) => {
+    return val**2;
+})
+console.log(newArr);
+
+// FILTER METHODS
+/*
+Create new array of elements that give true for a condition/filter
+eg:all even elements
+syntax :
+let newArr2 = arr.filter((val) => {
+    return val%2 === 0;
+    })
+*/
+
+let newArr2 = numArr.filter((val) => {
+    return val%2 === 0;
+})
+console.log(newArr2);
+
+// REDUCE METHOD
+/*
+Perfoms some ooperations & reduces the array to a single value. It returns that single value.
+example to find sum of numbers
+in callBack Function (result,value)
+*/
+const array = [1,2,3,4];
+const sumOfNum = array.reduce((res,curr) => {
+    return res + curr;
+});
+
+console.log(sumOfNum);
+
+let marks = [95,24,95,71,82,45,90];
+let Above90 = marks.filter((val) => {
+    return val > 90;
+});
+console.log(Above90);
+
+let userNum = prompt("Enter num :");
+let userArr = [];
+for(let i =0 ;i<userNum;i++){
+    userArr[i] = i+1;
+}
+console.log(userArr);
+
+let sumUserArr = userArr.reduce((res,val) => {
+    return res + val;
+});
+console.log(sumUserArr);
+
+let productUserArr = userArr.reduce((res,val) => {
+    return res * val;
+});
+console.log(productUserArr);

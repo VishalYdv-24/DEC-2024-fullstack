edeefa8994c0b5c79a18e08a4c5251336f6aba22
Vishal <vishal2004500@gmail.com>
2025-01-01 15:02:27 +0530

    Lecture 6:DOM-Document Object Model (Part1)

--- javascript/script.js
+++ javascript/script.js
@@ -1,158 +1,37 @@
-// Function & Methods
-/*Syntax:
-function functionName(){
-    write code here
+// let header = document.getElementById("heading");
+// console.log(header);
+// console.dir(header);
+// let tagname = document.getElementsByTagName("h1");
+// console.dir(tagname);
+
+
+// // Query Selector
+// // document.querySelector( #id/.class/tag ) to get the first element with the tag/class/id
+// // document.querySelectorAll( #id/.class/tag ) to get all the element with same tag/class/id
+// let elements = document.querySelector(".head");
+// console.log(elements);
+
+// let allElements = document.querySelectorAll(".head");
+// console.log(allElements);
+
+// // Properties
+// /*
+// tagName : returns tag for element nodes.
+// innerText : returns the text content of the element and all its children.
+// innerHTML : returns the plain text or HTML contents in the element. (with all tags same as html file)
+// textContent : returns textual content even for hidden elements.
+// */
+// // console.log(elements.tagName);
+// // console.log(elements.innerText);
+// // console.log(elements.innerHTML);
+// console.log(elements.textContent);
+// elements.innerText = "Bhavan's Junior College";
+
+let element = document.querySelector("h2");
+element.innerText = element.innerText + " from Apna College Student.";
+
+let num = 0;
+for(divs of document.querySelectorAll(".box")){
+    console.log(divs.innerText = "New Unique Value "+num);
+    num++;
 }
-
-to call function:
-
-functionName();
-*/
-function printinfo(name){
-    console.log(name,"Hello From Function block.");
-}
-function sum(num){
-    let s = 10;
-    s += num;
-    return s;
-}
-let num = 5;
-let name = "Vishal";
-printinfo(name);
-console.log(sum(num));
-
-// Arrow function : shorter form of function
-// Syntax : const functionName = (parameter1,2,3...) => {
-    // wirte code here
-// }
-
-const sum2 = (num) => {
-    return num+1;
-}
-console.log(sum2);
-console.log(sum2(num));
-
-// return number of vowels from string
-let str = "aeiouaeiouiiii";
-
-// using normal fuctions
-// function vowels(str){
-//     let vowelnum = 0;
-//     for(let i=0;i<str.length;i++){
-//         if('a' == str[i]){
-//             vowelnum++;
-//         }
-//         else if('e' == str[i]){
-//             vowelnum++;
-//         }
-//         else if('i' == str[i]){
-//             vowelnum++;
-//         }
-//         else if('o' == str[i]){
-//             vowelnum++;
-//         }
-//         else if('u' == str[i]){
-//             vowelnum++;
-//         }
-//     }
-//     return vowelnum;
-// }
-
-// using arrow function
-let vowels = (str) => {
-    let vowelnum = 0;
-    for(const char of str){
-        if(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
-            vowelnum++;
-        }
-    }
-    return vowelnum;
-}
-
-console.log(vowels(str));
-
-
-// forEach Loop in Arrays
-// Syntax : arr.forEach(callBackFunction)   callBackFunction : here,it is a function to execute for each elemnet in the array   *A callBackFunction passed as an argument to another function
-/*
-arr.forEach((value,index,array) =>{
-    console.log(val);
-})
-*/
-
-let arr = ["mumbai","delhi","pune","banglore"];
-arr.forEach(function(val,indx,arr){
-    console.log(val,indx,arr);
-})
-
-arr.forEach((val,indx,arr) => {
-    console.log(val.toUpperCase(),indx,arr);
-})
-
-let numArr = [1,2,3,4,5,6,7,8,9];
-numArr.forEach((val) => {
-    console.log(`${val}:${val*val}`);
-})
-
-// MAP METHOD 
-/*
-Creats a new array with results of some operations. The value its callback returns are used to form new array
-syntax:
-arr.map(callbackfnx(value,index,array))
-*/
-
-let newArr = numArr.map((val) => {
-    return val**2;
-})
-console.log(newArr);
-
-// FILTER METHODS
-/*
-Create new array of elements that give true for a condition/filter
-eg:all even elements
-syntax :
-let newArr2 = arr.filter((val) => {
-    return val%2 === 0;
-    })
-*/
-
-let newArr2 = numArr.filter((val) => {
-    return val%2 === 0;
-})
-console.log(newArr2);
-
-// REDUCE METHOD
-/*
-Perfoms some ooperations & reduces the array to a single value. It returns that single value.
-example to find sum of numbers
-in callBack Function (result,value)
-*/
-const array = [1,2,3,4];
-const sumOfNum = array.reduce((res,curr) => {
-    return res + curr;
-});
-
-console.log(sumOfNum);
-
-let marks = [95,24,95,71,82,45,90];
-let Above90 = marks.filter((val) => {
-    return val > 90;
-});
-console.log(Above90);
-
-let userNum = prompt("Enter num :");
-let userArr = [];
-for(let i =0 ;i<userNum;i++){
-    userArr[i] = i+1;
-}
-console.log(userArr);
-
-let sumUserArr = userArr.reduce((res,val) => {
-    return res + val;
-});
-console.log(sumUserArr);
-
-let productUserArr = userArr.reduce((res,val) => {
-    return res * val;
-});
-console.log(productUserArr);
